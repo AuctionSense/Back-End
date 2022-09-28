e9cd3e3186a1fb87ca3693958de6e042584cc076
JarnoStudent <481737@student.fontys.nl>
2022-09-28 19:25:06 +0200

    Initial setup with test

---
+++ quarkus-auctionsense/src/main/java/org/auctionsense/domain/Test.java
@@ -0,0 +1,49 @@
+package org.auctionsense.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Test {
+    @Id
+    private int id;
+    private String name;
+    private String email;
+    
+    public Test()
+    {
+
+    }
+
+    public Test(int id, String name, String email)
+    {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+}
