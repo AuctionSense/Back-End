0739307826418219c0545f039dff58fbfb4488c7
JarnoStudent <481737@student.fontys.nl>
2022-10-05 11:40:40 +0200

    Added import.sql file for mock data
    This file is only meant for development and can be removed in production.

---
+++ quarkus-auctionsense/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO `postgres`.`item` (`id`, `name`, `description`) VALUES ('0d313ace-b100-4a5e-82cd-59696de5e64c', 'testItemOne', 'This is the first item!');
+INSERT INTO `postgres`.`item` (`id`, `name`, `description`) VALUES ('2d313ace-b100-2a5e-82cd-59696de5e64c', 'testItemTwo', 'This is the second item!');
