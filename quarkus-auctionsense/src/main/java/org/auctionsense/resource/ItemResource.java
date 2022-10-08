4626b5717991ac4d516e2212ba2e3b10e3851cf9
JarnoStudent <481737@student.fontys.nl>
2022-10-08 17:25:42 +0200

    Added content type to itemResource.java

--- quarkus-auctionsense/src/main/java/org/auctionsense/resource/ItemResource.java
+++ quarkus-auctionsense/src/main/java/org/auctionsense/resource/ItemResource.java
@@ -3,16 +3,20 @@ package org.auctionsense.resource;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import org.auctionsense.domain.Item;
 import org.auctionsense.service.ItemService;
 
 @Path("/api/items")
+@Produces("application/json")
+@Consumes("application/json")
 public class ItemResource {
     @Inject
     private ItemService itemService;
