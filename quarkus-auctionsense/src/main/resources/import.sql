49c01863ebb52d3a8f0e3199fdc12776479a102f
JarnoStudent <481737@student.fontys.nl>
2022-10-09 14:48:30 +0200

    Added more mock data

--- quarkus-auctionsense/src/main/resources/import.sql
+++ quarkus-auctionsense/src/main/resources/import.sql
@@ -1,2 +1,3 @@
 INSERT INTO item (id, name, description, category) VALUES ('0d313ace-b100-4a5e-82cd-59696de5e64c', 'testItemOne', 'This is the first item!', 'games');
 INSERT INTO item (id, name, description, category) VALUES ('2d313ace-b100-2a5e-82cd-59696de5e64c', 'testItemTwo', 'This is the second item!', 'cars');
+INSERT INTO item (id, name, description, category) VALUES ('80a89e72-47d0-11ed-b878-0242ac120002', 'testItemThree', 'This is the third item!', 'games');
