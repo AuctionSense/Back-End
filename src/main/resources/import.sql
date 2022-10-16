1e1b0c2d6528cd7f03bc6f0c29f2b206e6073106
JarnoStudent <481737@student.fontys.nl>
2022-10-16 11:55:36 +0200

    Added library and removed import.sql

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO items (id, name, description, category) VALUES ('0d313ace-b100-4a5e-82cd-59696de5e64c', 'testItemOne', 'This is the first item!', 'games');
-INSERT INTO items (id, name, description, category) VALUES ('2d313ace-b100-2a5e-82cd-59696de5e64c', 'testItemTwo', 'This is the second item!', 'cars');
-INSERT INTO items (id, name, description, category) VALUES ('80a89e72-47d0-11ed-b878-0242ac120002', 'testItemThree', 'This is the third item!', 'games');
-
-INSERT INTO users (id, username, password, role) VALUES ('0d313ace-b100-4a5e-82cd-59696de5e64c', 'testUser', 'userPassword', 'user');
-INSERT INTO users (id, username, password, role) VALUES ('2d313ace-b100-2a5e-82cd-59696de5e64c', 'testAdmin', 'adminPassword', 'admin');
