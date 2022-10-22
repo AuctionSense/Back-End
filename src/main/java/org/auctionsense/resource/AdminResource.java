d8c4c7824160913b3a2d1b32fd8eecd456a4649e
JarnoStudent <481737@student.fontys.nl>
2022-10-22 18:19:00 +0200

    Role based endpoints get enforced

--- src/main/java/org/auctionsense/resource/AdminResource.java
+++ src/main/java/org/auctionsense/resource/AdminResource.java
@@ -2,14 +2,17 @@ package org.auctionsense.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import io.quarkus.security.Authenticated;
 
+@Path("/api/admin")
 @Authenticated
-@Path("/api/admins")
 public class AdminResource {
+
     @GET
-    @Path("/admin")
+    @Produces(MediaType.TEXT_PLAIN)
     public String admin() {
         return "granted";
     }
