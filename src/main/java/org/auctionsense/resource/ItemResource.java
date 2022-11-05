3ba5955f0d469fc7bc8dc3e291948534fb11a88e
Jarno Weemen <jarno.weemen@gmail.com>
2022-11-05 21:31:30 +0100

    Changed endpoint path.

--- src/main/java/org/auctionsense/resource/ItemResource.java
+++ src/main/java/org/auctionsense/resource/ItemResource.java
@@ -16,7 +16,7 @@ import javax.ws.rs.core.Response;
 import org.auctionsense.domain.Item;
 import org.auctionsense.service.ItemService;
 
-@Path("/api/items")
+@Path("/api/all/items")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ItemResource {
