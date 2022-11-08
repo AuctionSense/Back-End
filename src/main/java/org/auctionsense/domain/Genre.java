7f9339da01b1e5d87ca1e9d283ba327c999eefc1
Jarno Weemen <jarno.weemen@gmail.com>
2022-11-08 17:15:31 +0100

    Added getter and setter for list categories in genre.

--- src/main/java/org/auctionsense/domain/Genre.java
+++ src/main/java/org/auctionsense/domain/Genre.java
@@ -48,4 +48,13 @@ public class Genre {
     public void setName(String name) {
         this.name = name;
     }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
 }
